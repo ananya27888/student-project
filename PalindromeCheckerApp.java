9ebc22de65131042d1f3e67e4f126453adf57880
ananya <ananyakonatham27@gmail.com>
2026-02-13 11:00:30 +0530

    Added Palindrome Checker UC1

---
+++ PalindromeCheckerApp.java
@@ -0,0 +1,20 @@
+/**
+ * UseCase1PalindromeCheckerApp
+ * Version: 1.0
+ * Description: Displays welcome message and application details.
+ */
+
+public class UseCase1PalindromeCheckerApp {
+
+    static final String APP_VERSION = "1.0";
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Palindrome Checker");
+        System.out.println("Application Name: Palindrome Checker App");
+        System.out.println("Application Version: " + APP_VERSION);
+        System.out.println("Application started successfully.");
+        System.out.println("Ready for next use case...");
+    }
+}
+
