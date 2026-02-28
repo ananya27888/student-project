79d6e5d9b01c6f3ffa5396747de155f86dd72d02
ananya27888 <lakshmiananyakonatham@gmail.com>
2026-02-28 16:42:22 +0530

    Refine palindrome check output messages
    Updated output messages for palindrome check.

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,18 +1,18 @@
 public class UseCase2PalindromeCheckerApp {
 
     public static void main(String[] args) {
+
         String word = "madam";
         String reversed = "";
+
         for (int i = word.length() - 1; i >= 0; i--) {
             reversed = reversed + word.charAt(i);
         }
+
         if (word.equals(reversed)) {
-            System.out.println(word + " is a Palindrome.");
+            System.out.println("The word \"" + word + "\" is a palindrome.");
         } else {
-            System.out.println(word + " is NOT a Palindrome.");
+            System.out.println("The word \"" + word + "\" is not a palindrome.");
         }
-
-
-        
     }
 }
